d65beba96b22114d83a75cd057a32abc908f8a99
akr9757 <anilkumareddy9757@gmail.com>
2023-10-05 12:41:44 +0530

    terraform

--- vars.tf
+++ vars.tf
@@ -1,4 +1,7 @@
 variable "cidr_block" {}
-variable tags {}
+variable "tags" {}
 variable "env" {}
 variable "subnets" {}
+variable "default_vpc_id" {}
+variable "default_vpc_cidr" {}
+variable "default_vpc_rtid" {}
